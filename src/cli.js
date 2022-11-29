a9889c331634766662765157d22d24b77dd85b74
Anton Zvyagintsev <antonzvyagi@gmail.com>
2022-11-30 02:58:53 +0300

    add greeting

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+const start = function () {
+    console.log("Welcome to the Brain Games!")
+    var userName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${userName}!`);
+};
+
+export default {
+    start
+};
